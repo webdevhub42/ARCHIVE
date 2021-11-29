af820efe9aea52bf7a202ccaf625d5a395604bb5
webdevhub42 <webdevresourcehub@gmail.com>
2021-11-29 02:00:42 +0000

    -update

---
+++ future-work-master/future-work-master/future-unsorted/medium-clone-project-main/app.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+const { ValidationError } = require('sequelize');
+const { environment } = require('./config');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/api/users');
+const responsesRouter = require('./routes/api/responses');
+const storiesRouter = require('./routes/api/stories');
+const cookieParser = require('cookie-parser');
+// const csrfProtection = require('./routes/csrf');
+const app = express();
+
+app.set('view engine', 'pug');
+app.use(cookieParser());
+app.use(morgan('dev'));
+app.use(express.urlencoded());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+
+app.use('/', indexRouter);
+app.use('/api/stories', storiesRouter); // delete csrf for testing purposes
+app.use('/api/users', usersRouter);
+app.use('/api/responses', responsesRouter);
+
+app.use((req, res, next) => {
+  const err = new Error('The requested resource couldn\'t be found.');
+  err.errors = ['The requested resource couldn\'t be found.'];
+  err.status = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  if (err instanceof ValidationError) {
+    err.errors = err.errors.map(e => e.message);
+    err.title = 'Sequelize Error';
+  }
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(err.status || 500);
+  const acceptHeader = req.get('Accept');
+  const isProduction = environment === 'production';
+
+  const errorData = {
+    title: err.title || 'Server Error',
+    message: err.message,
+    stack: isProduction ? null : err.stack,
+    errors: err.errors
+  };
+
+  if (acceptHeader.includes('text/html')) {
+    res.render('errors', errorData);
+  } else if (acceptHeader === 'application/json') {
+    res.json(errorData);
+  } else {
+    res.send('Server Error');
+  }
+});
+
+
+module.exports = app;
